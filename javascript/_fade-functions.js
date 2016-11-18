7c6af00bcde2d27b25e10801980275fc4f0fd256
Alex Devero <deveroalex@gmail.com>
2016-11-18 09:55:53 +0100

    Add fade in and out functions

--- javascript/_fade-functions.js
+++ javascript/_fade-functions.js
@@ -1,1 +1,33 @@
+// Fade in function
+const fadeIn = (element) => {
+  let elementOpacity = 0.1;// initial opacity
 
+  element.style.display = 'block';
+
+  let timer = setInterval(function () {
+    if (elementOpacity >= 1){
+      clearInterval(timer);
+    }
+
+    element.style.opacity = elementOpacity;
+
+    elementOpacity += elementOpacity * 0.1;
+  }, 15);
+}
+
+// Fade out function
+const fadeOut = (element) => {
+  let elementOpacity = 1;// initial opacity
+
+  let timer = setInterval(function () {
+    if (elementOpacity <= 0.1){
+      clearInterval(timer);
+
+      element.style.display = 'none';
+    }
+
+    element.style.opacity = elementOpacity;
+
+    elementOpacity -= elementOpacity * 0.1;
+  }, 15);
+}
